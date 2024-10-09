03888dd39137d0b85436f88686358eb3f6dbe6d8
Michael Fehrer <112262472+mtfehrer@users.noreply.github.com>
2024-10-09 18:47:10 -0400

    add message data class

---
+++ lib/src/data/message.dart
@@ -0,0 +1,20 @@
+import "utils.dart";
+
+class Message {
+  final String id;
+  final String content;
+  final String author;
+
+  Message({required this.id, required this.content, required this.author});
+
+  Message.fromJson(Json json) :
+	id = json["id"],
+	content = json["content"],
+	author = json["author"]
+
+  Json toJson() => {
+	"id": id,
+	"content": content,
+	"author": author,
+  };
+}
