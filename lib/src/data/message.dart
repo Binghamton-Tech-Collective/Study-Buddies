ce45b49ad1c67cf34fe544fa436f0eaf5ca428aa
Michael Fehrer <112262472+mtfehrer@users.noreply.github.com>
2024-11-07 18:39:56 -0500

    update types of message data class fields and add timestamp

--- lib/src/data/message.dart
+++ lib/src/data/message.dart
@@ -1,22 +1,26 @@
 import "utils.dart";
 
 extension type MessageId(String value) {}
+extension type UserID(String value) {}
 
 class Message {
-  final String id;
+  final MessageId id;
   final String content;
-  final String author;
+  final UserID author;
+  final DateTime timestamp;
 
-  Message({required this.id, required this.content, required this.author});
+  Message({required this.id, required this.content, required this.author, required this.timestamp});
 
   Message.fromJson(Json json) :
 	id = json["id"],
 	content = json["content"],
-	author = json["author"];
+	author = json["author"],
+	timestamp = json["timestamp"];
 
   Json toJson() => {
 	"id": id,
 	"content": content,
 	"author": author,
+	"timestamp": timestamp,
   };
 }
